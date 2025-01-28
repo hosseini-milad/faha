10ddacc89e874da1ec482d68c170df3eb31fbf6f
milad <milad.cia@gmail.com>
2025-01-28 17:04:07 +0330

    fetch

--- middleware/Calc/FindSimilar.js
+++ middleware/Calc/FindSimilar.js
@@ -4,8 +4,7 @@ const FindSimilar=(array,item)=>{
     if(!array||!array.length) return(0)
     if(!item) return(0)
     for(var i=0;i<array.length;i++){
-        if(array[i] == item) return(1)
-        if(array[i].value&&array[i].enTitle==item.value) return(1)
+        if(array[i].value&&array[i].value==item.value) return(1)
     }
 return(0)
 }

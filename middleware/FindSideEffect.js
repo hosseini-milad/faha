201c945dea0a8a189ac973c25886eae4dbdf2f19
milad.cia <milad.cia@gmail.com>
2025-02-18 15:39:16 +0330

    group

--- middleware/FindSideEffect.js
+++ middleware/FindSideEffect.js
@@ -1,10 +1,11 @@
 const faktor = require("../models/product/faktor");
-const faktorItems = require("../models/product/faktorItems")
+const faktorItems = require("../models/product/faktorItems");
+const GetHesabFa = require("./GetHesabFa");
 
 var ObjectID = require('mongodb').ObjectID;
 
 const FindSideEffect=async(data,body)=>{
-var error=''
+    var error=''
     if(body.status=="senttofactory"){
         if(!body.factory){
             return({error:"تولید کننده وارد نشده است"})
@@ -27,6 +28,54 @@ var error=''
         if(!body.transportCode&&!peyk){
             return({error:"اطلاعات ارسال وارد نشده است"})
         }
+    }
+    if(body.status=="archive"){
+        const query = {
+            invoice:{
+                reference: '',
+                date: '2018-07-17 17:30:12',
+                dueDate: '2018-07-17 17:30:12',
+                contactCode: '000001',
+                contactTitle: 'سعید محمدی',
+                note: '',
+                sent: false,
+                invoiceType: 0,
+                status: 2,
+                tag: '',
+                freight: 0,
+                freightPersonCode: '',
+                warehouseReceiptStatus: 1,
+                project: 'پروژه یک',
+                salesmanCode: 10001,
+                salesmanPercent: 30,
+                currency: 'IRR',
+                invoiceItems: [{
+                    rowNumber: 1,
+                    description: 'Galaxy J7 گوشی سامسونگ',
+                    itemCode: '000001',
+                    unit: 'عدد',
+                    quantity: 1,
+                    unitPrice: 100000,
+                    discount: 0,
+                    tax: 9000,
+                    serialNumbers: ['974900098723']
+                }],
+            others : [
+                {
+                    "title": "هزینه های گمرکی",
+                    "amount": 1540000.0,
+                    "add": true 
+                }
+            ],
+            currency: "IRR",
+            taxId: "",
+            currencyRate: 1.0000000000
+            }
+        }
+        const hesabResult = 0&&await GetHesabFa(query,"/invoice/save")
+        
+            return({error:query})
+        
     }
     return(1)
 }

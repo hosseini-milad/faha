fd4251add1c47392d6688424fc026db36e1bd365
milad.cia <milad.cia@gmail.com>
2025-02-18 10:56:25 +0330

    group

--- middleware/FindNextStatus.js
+++ middleware/FindNextStatus.js
@@ -10,13 +10,14 @@ const FindNextStatus=async(faktorItem,body)=>{
     var nextStatus= faktorItem.status
     if(body.value=1){
         var nextIndex = index
-        if(index==0) nextIndex= 2
-        if(index==1) nextIndex= 2
+        if(index==0) nextIndex= 3
+        if(index==1) nextIndex= 3
         if(index==2) nextIndex=3
         if(index==3) nextIndex=4
         if(index==4) nextIndex=5
         if(index==5) nextIndex=6
         if(index==6) nextIndex=7
+        if(index==7) nextIndex=8
         var nextStatus= faktorItem.status
         try{
             nextStatus=crmSteps[nextIndex]

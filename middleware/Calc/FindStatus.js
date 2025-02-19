023db86085a9d0c2b08bce747986b53a35d574df
milad.cia <milad.cia@gmail.com>
2025-02-19 09:52:42 +0330

    status

--- middleware/Calc/FindStatus.js
+++ middleware/Calc/FindStatus.js
@@ -1,9 +1,14 @@
 const crmlist = require("../../models/crm/crmlist")
 
-const FindStatus=async(enStep)=>{
+const FindStatus=async(enStep,limit)=>{
     const crmData = await crmlist.findOne()
     if(!crmData) return('')
-    const steps = crmData.crmSteps
+    var steps = crmData.crmSteps
+    if(limit){
+        if(enStep == "quote" || enStep=="edit")  enStep ="inprogress"
+        if(enStep == "prepair" || enStep == "pay")  enStep ="accept"
+        if(enStep == "done" || enStep == "archive")  enStep ="send"
+    }
     const faStep = steps.find(item=>item.enTitle==enStep)
     if(faStep){
         return(faStep.title)

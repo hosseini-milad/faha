707d09752ba6fe86f11558eff80d178a5d6690c1
milad <milad.cia@gmail.com>
2025-02-26 18:18:45 +0330

    buttons

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -675,7 +675,7 @@ router.post('/cart-to-faktor-from',auth,jsonParser, async (req,res)=>{
             const productDetail = await products.findOne({sku:cartItem.sku})
             
             const { _id: _, ...newObj } = cartItem;
-            var status = "inprogress"
+            var status = "pay"
             await faktorItems.create({...newObj,faktorNo:faktorNo,
                 status:status,cartDetail,
                 cName:userData.username,phone:userData.phone})

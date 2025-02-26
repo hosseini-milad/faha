9618e58a375416a32d09365290bdb7b935c82a9e
milad <milad.cia@gmail.com>
2025-02-26 18:15:47 +0330

    buttons

--- models/product/Colors.js
+++ models/product/Colors.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ColorSchema = new mongoose.Schema({
     title:  String,
     enTitle:String,
+    active:Boolean,
     colorCode:String
 })
 module.exports = mongoose.model('colors',ColorSchema);

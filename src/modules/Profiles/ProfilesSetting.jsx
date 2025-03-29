121514d6a605c2272576873728334dcb44c4ce73
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2025-03-29 16:06:42 +0330

    new

--- src/modules/Profiles/ProfilesSetting.jsx
+++ src/modules/Profiles/ProfilesSetting.jsx
@@ -36,7 +36,7 @@ const ProfilesSetting = (props) => {
       url: "/panel/user/update-user",
       body: { _id: userId, ...Changes },
     });
-    // setTimeout(() => (window.location.reload(), 3000));
+    setTimeout(() => (window.location.reload(), 3000));
   };
 
   return (

d31a2102f72bc3c371a63f938757b2081f9372fa
milad.cia <milad.cia@gmail.com>
2025-04-05 09:22:49 +0330

    accept type

--- middleware/FindAccess.js
+++ middleware/FindAccess.js
@@ -3,10 +3,15 @@ const ProfileAccess = require("../models/auth/ProfileAccess")
 var ObjectID = require('mongodb').ObjectID;
 
 const FindAccess=async(user)=>{
-    if(!user) return([])
-    const profileData = await ProfileAccess.findOne({_id: ObjectID(user)})
-    if(!profileData) return([])
-    const profileAccess = profileData.access
-    return(profileAccess)
+    try{
+        if(!user) return([])
+        const profileData = await ProfileAccess.findOne({_id: ObjectID(user)})
+        if(!profileData) return([])
+        const profileAccess = profileData.access
+        return(profileAccess)
+    }
+    catch{
+        return([])
+    }
 }
 module.exports =FindAccess

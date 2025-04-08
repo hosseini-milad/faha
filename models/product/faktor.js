1f4368c6ce8a689d639b55fee866611dea525c42
milad.cia <milad.cia@gmail.com>
2025-04-08 12:25:48 +0330

    thumb to icon

--- models/product/faktor.js
+++ models/product/faktor.js
@@ -17,6 +17,7 @@ const FaktorSchema = new Schema({
     status:{ type: String },
 
     totalPrice:{ type: String },
+    discount:{ type: String },
     totalDiscount:{ type: String },
  
     transportWay:{ type: String },

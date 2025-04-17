7f7c16f9af9630144be4df5acac5ab7992a42b77
milad.cia <milad.cia@gmail.com>
2025-04-17 10:34:49 +0330

    discount

--- middleware/Calc/CheckAllRecieve.js
+++ middleware/Calc/CheckAllRecieve.js
@@ -5,6 +5,7 @@ const CheckAllRecieve=async(faktorNo)=>{
     if(!faktorItemsList||!faktorItemsList.length) return(0)
     
     for(var i=0;i<faktorItemsList.length;i++){
+        console.log(faktorItemsList[i].isRecieved)
         if(!faktorItemsList[i].isRecieved){
             return(0) 
         }

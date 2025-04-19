2180cea5aabf7352585816e5e45d6d718fc3920c
milad.cia <milad.cia@gmail.com>
2025-04-19 10:37:26 +0330

    All recieve

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -1168,7 +1168,7 @@ router.post('/update-faktor',jsonParser,auth, async (req,res)=>{
             }
         }
         const faktorData = await CalcFaktorData(faktorNo)
-        res.json({...faktorData})
+        res.json({data:faktorData})
     }
     catch(error){
         res.status(500).json({message: error.message})

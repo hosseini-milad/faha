4826d4af6d769f3e42dcc7a5c729e21e0e804a0c
hosseini.milad1400 <milad.cia@gmail.com>
2025-04-24 22:59:09 +0330

    csv

--- middleware/Calc/DataToCSV.js
+++ middleware/Calc/DataToCSV.js
@@ -1,6 +1,15 @@
-const NormalNumber = require("../NormalNumber")
+
+var fs = require('fs');
 
 const DataToCSV=async(data)=>{
+
+fs.writeFile('/upload/csv/result.csv', data, 'utf8', function (err) {
+  if (err) {
+    console.log('Some error occured - file either not saved or corrupted file saved.');
+  } else{
+    console.log('It\'s saved!');
+  }
+});
     return("https://admin.fahascrubs.com/upload/csv/result.csv")
 }
 

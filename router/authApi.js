890e44c13f2a2acd15efc2b8709d17873e849e58
hosseini.milad1400 <milad.cia@gmail.com>
2025-06-03 12:10:20 +0330

    logo update

--- router/authApi.js
+++ router/authApi.js
@@ -151,7 +151,10 @@ router.post('/customer-otp',jsonParser,async(req,res)=>{
         token: otpValue,
         template: process.env.template,//"mgmVerify",
         receptor: phone 
-    },);
+    },function(response, status) {
+        console.log(response);
+        console.log(status);
+    });
       const newUser = await customers.create(
         { username:phone,
           phone:phone,
